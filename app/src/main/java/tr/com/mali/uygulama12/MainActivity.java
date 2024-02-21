bf5d61db7deefebd81247af866b3ceb91d87775f
muhammed380 <ibicioglumuhammetali@gmail.com>
2024-02-21 12:01:12 +0300

    isim listeleme

---
+++ app/src/main/java/tr/com/mali/uygulama12/MainActivity.java
@@ -0,0 +1,43 @@
+package tr.com.mali.uygulama12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonEkle;
+    EditText editTextAd;
+    ListView listViewAdlar;
+    ArrayList<String> isimlerListesi = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, isimlerListesi);
+        buttonEkle = findViewById(R.id.buttonEkle);
+        editTextAd = findViewById(R.id.editTextAd);
+        listViewAdlar = findViewById(R.id.listViewAdlar);
+        listViewAdlar.setAdapter(adapter);
+        buttonEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad = editTextAd.getText().toString();
+                isimlerListesi.add(ad);
+                adapter.notifyDataSetChanged();
+                editTextAd.getText().clear();
+            }
+
+
+        });
+
+    }
+}
